7c64431ec8d27bda56f12615da5175de6bea93fc
Ankoongggg <annvthe181807@fpt.edu.vn>
2026-04-20 23:22:11 +0700

    Update logic booking + validate

--- routes/owner/index.js
+++ routes/owner/index.js
@@ -8,11 +8,13 @@ const express = require('express');
 const ownerPostsRoutes = require('./posts');
 const ownerBookingRoutes = require('./bookingRoutes');
 const ownerWalletRoutes = require('./wallet');
+const ownerServiceBookingRoutes = require('./serviceBookingRoutes');
 
 const router = express.Router();
 
 router.use('/posts', ownerPostsRoutes);
 router.use('/bookings', ownerBookingRoutes);
+router.use('/service-bookings', ownerServiceBookingRoutes);
 router.use('/', ownerWalletRoutes);
 
 module.exports = router;
