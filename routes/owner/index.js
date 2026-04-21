321819cd3b00b22a869cc382ec52e3378436ddae
sangcoi0511 <sang07626@gmail.com>
2026-04-22 01:30:14 +0700

    Add report management functionality with new routes and controller

--- routes/owner/index.js
+++ routes/owner/index.js
@@ -9,12 +9,14 @@ const ownerPostsRoutes = require('./posts');
 const ownerBookingRoutes = require('./bookingRoutes');
 const ownerWalletRoutes = require('./wallet');
 const ownerServiceBookingRoutes = require('./serviceBookingRoutes');
+const ownerReportsRoutes = require('./reports');
 
 const router = express.Router();
 
 router.use('/posts', ownerPostsRoutes);
 router.use('/bookings', ownerBookingRoutes);
 router.use('/service-bookings', ownerServiceBookingRoutes);
+router.use('/reports', ownerReportsRoutes);
 router.use('/', ownerWalletRoutes);
 
 module.exports = router;
